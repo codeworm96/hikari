cfd0642e4734f5752073d0cf9301e8a4fc62559f
Yuning Zhang <codeworm96@outlook.com>
2019-01-04 16:30:23 -0500

    :sparkles: Add Vec3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use image::{ImageBuffer, Rgb};
 
+mod vec3;
+use crate::vec3::Vec3;
+
 const W: u32 = 200;
 const H: u32 = 100;
 
@@ -7,12 +10,10 @@ fn main() {
     let mut img = ImageBuffer::from_pixel(W, H, Rgb([0u8, 0u8, 0u8]));
     for x in 0..W {
         for y in 0..H {
-            let r = x as f64 / W as f64;
-            let g = 1.0 - y as f64 / H as f64;
-            let b = 0.2;
-            let ir = (r * 255.99) as u8;
-            let ig = (g * 255.99) as u8;
-            let ib = (b * 255.99) as u8;
+            let color = Vec3::new(x as f64 / W as f64, 1.0 - y as f64 / H as f64, 0.2);
+            let ir = (color.r() * 255.99) as u8;
+            let ig = (color.g() * 255.99) as u8;
+            let ib = (color.b() * 255.99) as u8;
             img.put_pixel(x, y, Rgb([ir, ig, ib]));
         }
     }
---
+++ src/vec3.rs
@@ -0,0 +1,23 @@
+pub struct Vec3 {
+    e: [f64; 3],
+}
+
+impl Vec3 {
+    pub fn zero() -> Vec3 {
+        Vec3 { e: [0.0; 3] }
+    }
+
+    pub fn new(e0: f64, e1: f64, e2: f64) -> Vec3 {
+        Vec3 { e: [e0, e1, e2] }
+    }
+
+    pub fn r(&self) -> f64 {
+        self.e[0]
+    }
+    pub fn g(&self) -> f64 {
+        self.e[1]
+    }
+    pub fn b(&self) -> f64 {
+        self.e[2]
+    }
+}
