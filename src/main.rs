d879c75a0601791e41c07e30f7686530ce71e78f
Yuning Zhang <codeworm96@outlook.com>
2019-01-05 00:03:31 -0500

    :sparkles: Add Camera

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use image::{ImageBuffer, Rgb};
 
+mod camera;
 mod hitable;
 mod hitable_list;
 mod ray;
 mod sphere;
 mod vec3;
+use crate::camera::Camera;
 use crate::hitable::{HitRecord, Hitable};
 use crate::hitable_list::HitableList;
 use crate::ray::Ray;
@@ -30,19 +32,16 @@ fn color(r: &Ray, world: &dyn Hitable) -> Vec3 {
 
 fn main() {
     let mut img = ImageBuffer::from_pixel(W, H, Rgb([0u8, 0u8, 0u8]));
-    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
-    let horizontal = Vec3::new(4.0, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, 2.0, 0.0);
-    let origin = Vec3::new(0.0, 0.0, 0.0);
     let world = HitableList::new(vec![
         Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)),
         Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)),
     ]);
+    let cam = Camera::new();
     for x in 0..W {
         for y in 0..H {
             let u = x as f64 / W as f64;
             let v = 1.0 - y as f64 / H as f64;
-            let r = Ray::new(origin, lower_left_corner + horizontal * u + vertical * v);
+            let r = cam.get_ray(u, v);
             let col = color(&r, &world);
             let ir = (col.r() * 255.99) as u8;
             let ig = (col.g() * 255.99) as u8;
