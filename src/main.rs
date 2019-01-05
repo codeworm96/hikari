3efa277bff6e67e0c5b68f1e59d8b7f8dd37778c
Yuning Zhang <codeworm96@outlook.com>
2019-01-04 21:47:14 -0500

    :sparkles: Add normal

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,24 @@ use crate::vec3::{dot, Vec3};
 const W: u32 = 200;
 const H: u32 = 100;
 
-fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> f64 {
     let oc = *r.origin() - *center;
     let a = dot(r.direction(), r.direction());
     let b = 2.0 * dot(&oc, r.direction());
     let c = dot(&oc, &oc) - radius * radius;
     let d = b * b - 4.0 * a * c;
-    d > 0.0
+    if d < 0.0 {
+        -1.0
+    } else {
+        (-b - d.sqrt()) / (2.0 * a)
+    }
 }
 
 fn color(r: &Ray) -> Vec3 {
-    if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r) {
-        Vec3::new(1.0, 0.0, 0.0)
+    let t = hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = (r.point_at_parameter(t) - Vec3::new(0.0, 0.0, -1.0)).unit();
+        Vec3::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0) * 0.5
     } else {
         let unit_direction = r.direction().unit();
         let t = 0.5 * (unit_direction.y() + 1.0);
